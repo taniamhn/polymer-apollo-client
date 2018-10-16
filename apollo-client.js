fa2fe657b029dd4533f7e5f0d6be48ec1f8830f5
Tania Melissa Hernandez Narvaez <taniamhn@gmail.com>
2018-10-16 08:57:29 -0500

    cosmodox build

--- apollo-client.js
+++ apollo-client.js
@@ -1,24 +1,4 @@
-import ApolloClient, { getFragmentDefinitions, createFragmentMap } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { BatchHttpLink } from "apollo-link-batch-http";
+import ApolloClient from "apollo-boost";
 import gql from 'graphql-tag';
 
-/**
- * @param config
- * @returns {ApolloClient}
- */
-const init = (config) => {
-  if (! config) {
-    console.error('Trying to initialize an ApolloClient without having a config set');
-  }
-
-  // Create the apollo client
-  return new ApolloClient({
-    link: new BatchHttpLink(config),
-    cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
-  });
-}
-
-const namedClient = {};
-
-export { init, gql, getFragmentDefinitions, createFragmentMap, namedClient };
+export { gql, ApolloClient };
