a094b87e3168baac60061e24925ba9f4f1bdf597
Tania Melissa Hernandez Narvaez <taniamhn@gmail.com>
2018-10-22 15:53:48 -0500

    added introspectionMatcher for inline fragments

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,9 @@
 import ApolloClient from "apollo-client";
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
+import { createUploadLink } from 'apollo-upload-client';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import gql from 'graphql-tag';
 
-export { gql, ApolloClient, InMemoryCache, HttpLink, onError, ApolloLink };
+export { gql, ApolloClient, InMemoryCache, HttpLink, onError, ApolloLink, IntrospectionFragmentMatcher };
