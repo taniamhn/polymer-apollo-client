5b9c9661c01d80737f9c6d2ff5d69cbbd6f284aa
Tania Melissa Hernandez Narvaez <taniamhn@gmail.com>
2019-01-22 18:21:57 -0500

    updated to polymer 3

--- apollo-client.js
+++ apollo-client.js
@@ -5,34 +5,34 @@ import { HttpLink } from 'apollo-link-http';
 import gql from 'graphql-tag';
 
 const errors = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.map(({ message, locations, path }) => {
-      console.error(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`);
-    });
-  }
+    if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) => {
+            console.error(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`);
+        });
+    }
 
-  if (networkError) {
-    console.error(`[Network error]: ${networkError}`);
-  }
+    if (networkError) {
+        console.error(`[Network error]: ${networkError}`);
+    }
 });
 
 const init = (config, wsConfig, apolloConfig) => {
-  if (!config) {
-    console.warn('Trying to initialize ApolloClient without config property, default config will be used.');
-  } else if (!config.uri) {
-    console.warn('Trying to initialize ApolloClient without config.uri property, default config.uri will be used.');
-  }
+    if (!config) {
+        console.warn('Trying to initialize ApolloClient without config property, default config will be used.');
+    } else if (!config.uri) {
+        console.warn('Trying to initialize ApolloClient without config.uri property, default config.uri will be used.');
+    }
 
-  const link = new HttpLink(config);
+    const link = new HttpLink(config);
 
-  const cache = new InMemoryCache().restore(window.__APOLLO_STATE__);
+    const cache = new InMemoryCache().restore(window.__APOLLO_STATE__);
 
-  const fullConfig = Object.assign({}, {
-    link: link,
-    cache: cache
-  }, apolloConfig);
+    const fullConfig = Object.assign({}, {
+        link: link,
+        cache: cache
+    }, apolloConfig);
 
-  return new ApolloClient(fullConfig);
+    return new ApolloClient(fullConfig);
 }
 
 const namedClient = {};
--- graphql-client.js
+++ graphql-client.js
@@ -1,6 +1,4 @@
-<link rel="import" href="../polymer/polymer-element.html">
-
-<!--
+/**
 ## GraphQL client
 
 Use to create a connection to your GraphQL endpoint.
@@ -58,63 +56,67 @@ and at the `webpack.config.js` to see how to compile your version.
 @customElement
 @demo demo/graphql-query-simple.html Simple query
 @demo demo/full-demo.html Full demo
--->
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-<script>
-  const CLIENT_NAME_DEFAULT = 'default';
+export const CLIENT_NAME_DEFAULT = 'default';
 
-  class GraphQLClient extends Polymer.Element {
-    static get is() {
-      return 'graphql-client';
-    }
-
-    static get properties() {
-      return {
-        /**
-         * Will be passed to `HttpLink()`.
-         */
-        config: {
-          type: Object
-        },
-
-        /**
-         * Will be passed to `WebSocketLink()`.
-         */
-        wsConfig: {
-          type: Object
-        },
-
-        /**
-         * Will be passed to `ApolloClient()`.
-         */
-        apolloConfig: {
-          type: Object
-        },
-
-        /**
-         * Name of the client. When using only a single endpoint in your app, you can omit this parameter.
-         */
-        clientName: {
-          type: String,
-          value: CLIENT_NAME_DEFAULT
-        }
-      };
-    }
-
-    /**
-     * @protected
-     */
-    connectedCallback() {
-      super.connectedCallback()
-
-      window.Apollo.namedClient[this.clientName] = window.Apollo.init(this.config, this.wsConfig, this.apolloConfig);
-
-      if (this.clientName === CLIENT_NAME_DEFAULT) {
-        window.Apollo.client = window.Apollo.namedClient[CLIENT_NAME_DEFAULT]
-        window.__APOLLO_CLIENT__ = window.Apollo.client
-      }
-    }
+class GraphQLClient extends PolymerElement {
+  static get is() {
+    return 'graphql-client';
   }
 
-  customElements.define(GraphQLClient.is, GraphQLClient);
-</script>
+  static get properties() {
+    return {
+      /**
+       * Will be passed to `HttpLink()`.
+       */
+      config: {
+        type: Object
+      },
+
+      /**
+       * Will be passed to `WebSocketLink()`.
+       */
+      wsConfig: {
+        type: Object
+      },
+
+      /**
+       * Will be passed to `ApolloClient()`.
+       */
+      apolloConfig: {
+        type: Object
+      },
+
+      /**
+       * Name of the client. When using only a single endpoint in your app, you can omit this parameter.
+       */
+      clientName: {
+        type: String,
+        value: CLIENT_NAME_DEFAULT
+      }
+    };
+  }
+
+  /**
+   * @protected
+   */
+  connectedCallback() {
+    super.connectedCallback()
+
+    window.Apollo.namedClient[this.clientName] = window.Apollo.init(this.config, this.wsConfig, this.apolloConfig);
+
+    if (this.clientName === CLIENT_NAME_DEFAULT) {
+      window.Apollo.client = window.Apollo.namedClient[CLIENT_NAME_DEFAULT]
+      window.__APOLLO_CLIENT__ = window.Apollo.client
+    }
+  }
+}
+
+customElements.define(GraphQLClient.is, GraphQLClient);
